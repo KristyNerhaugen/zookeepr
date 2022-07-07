819b67bf2d4d7d4159a1766b1c338ea06086e4e1
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-07 17:20:14 -0500

    refactored

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data 
