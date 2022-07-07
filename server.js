52a8c4b49f29bf1879ae55dec3bc383e45d5bb9c
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-07 18:48:30 -0500

    fixed order in server js file to now read html and css properly

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data 
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-app.use(express.static('public'));
 const { animals } = require('./data/animals');
 const { get } = require('http');
 
